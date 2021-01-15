5efe68490b4eda380c70f64306ede580030afd5c
Peter Ihimire <peterihimire@gmail.com>
2021-01-15 09:27:58 +0100

    Worked on the Banner component, again I used the react hooks useEffect for this one instead of the componentDidMount

---
+++ src/components/Banner.js
@@ -0,0 +1,137 @@
+import React from "react";
+import "./Banner.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import bannerOne from "../assets/man-and-lady.png";
+import bannerTwo from "../assets/man-and-lady.png";
+
+const Banner = () => {
+  React.useEffect(() => {
+    // header carousel
+    const headerCarousel = document.querySelector(".carousel");
+    const carouselH = document.querySelector(".carousel.carousel-slider");
+    const carouselVx = document.querySelector(
+      ".carousel.carousel-slider.visible-xs.mbxs"
+    );
+    M.Carousel.init(headerCarousel, {});
+    M.Carousel.init(carouselH, {
+      fullWidth: true,
+    });
+    M.Carousel.init(carouselVx, {
+      fullWidth: false,
+    });
+
+    window.onload = function () {
+      initCarousel(".carousel", {
+        fullWidth: true,
+        // indicators: true,
+        autoScroll: 5000, // time in ms
+      });
+    };
+
+    function initCarousel(elms, opts) {
+      if (!window || !document) return null;
+
+      const instances = M.Carousel.init(getElements(elms), opts);
+
+      if (Array.isArray(instances)) {
+        for (let i = 0; i < instances.length; ++i) {
+          addAutoScroll(instances[i]);
+        }
+      } else {
+        addAutoScroll(instances);
+      }
+
+      return instances;
+    }
+
+    function addAutoScroll(instance) {
+      if (!instance.options.autoScroll) return;
+
+      instance.autoScrollIntervalId = window.setInterval(() => {
+        instance.next();
+      }, instance.options.autoScroll);
+    }
+
+    function getElements(elms) {
+      if (elms.charAt(0) === "#") {
+        elms = elms.replace("#", "");
+        return document.getElementById(elms);
+      }
+
+      return document.querySelectorAll(elms);
+    }
+  });
+
+  return (
+    <section className="home">
+      <div className="row full-height">
+        <div className="container full-height">
+          <div className="carousel carousel-slider full-height">
+            <div className="carousel-item">
+              <div className="col s12 pad0 center-xs">
+                <div className="col s12 m7 l5 ">
+                  <div className="main-text-container">
+                    <h1>Amazing Experiences from Our Wonderful Customers</h1>
+                    <p className="hidden-xs">
+                      Here is what customers and vendors are saying about us,
+                      you can share your stories with us too.
+                    </p>
+                    <div className="btn hidden-xs btn-bigger">buy a car</div>
+                  </div>
+                </div>
+                <div className="col s12 m5 offset-m0 l5 offset-l2">
+                  <div className="rel right-align banner-home-img">
+                    <img
+                      src={bannerTwo}
+                      alt="banner-img-1"
+                      className="full-width home-img"
+                    />
+                  </div>
+                </div>
+                <div className="col s12 visible-xs">
+                  <p>
+                    Here is what customers and vendors are saying about us, you
+                    can share your stories with us too.
+                  </p>
+                  <div className="btn btn-bigger ">buy a car</div>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <div className="col s12 pad0 center-xs">
+                <div className="col s12 m7 l5">
+                  <div className="main-text-container">
+                    <h1>Amazing Experiences from Our Wonderful Customers</h1>
+                    <p className="hidden-xs">
+                      Here is what customers and vendors are saying about us,
+                      you can share your stories with us too.
+                    </p>
+                    <div className="btn hidden-xs btn-bigger">buy a car</div>
+                  </div>
+                </div>
+                <div className="col s12 m5 offset-m0 l5 offset-l2">
+                  <div className="rel right-align banner-home-img">
+                    <img
+                      src={bannerOne}
+                      alt="banner-img-1"
+                      className="full-width home-img"
+                    />
+                  </div>
+                </div>
+                <div className="col s12 visible-xs">
+                  <p>
+                    Here is what customers and vendors are saying about us, you
+                    can share your stories with us too.
+                  </p>
+                  <div className="btn btn-bigger">buy a car</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
