4ae8408555a11a239e1cbed9305951121ab7c8ff
Peter Ihimire <peterihimire@gmail.com>
2021-01-16 16:59:59 +0100

    Working on the banner animation with gsap, then editted some content in the index file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,8 +3,57 @@ import "./Banner.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 // import bannerOne from "../assets/man-and-lady.png";
 import bannerTwo from "../assets/man-and-lady.png";
+import { TweenMax, TimelineLite, Power3 } from "gsap";
 
 const Banner = () => {
+  let hero = React.useRef(null);
+  let heroImg = React.useRef(null);
+  let heroHeading = React.useRef(null);
+  let heroMobile = React.useRef(null);
+
+  let tl = new TimelineLite();
+
+  React.useEffect(() => {
+    // HEADINGS
+    console.log(heroHeading);
+
+    const headingImg = heroImg;
+    const headingFirst = heroHeading.children[0].children[0];
+    const headingSecond = headingFirst.nextSibling;
+    const headingThird = headingSecond.nextSibling;
+    const headingFourth = headingThird.nextSibling;
+    const headingP = heroHeading.children[1];
+    const headingBtn = heroHeading.children[2];
+    const heroMobHead = heroMobile.children[0];
+    const heroMobP = heroMobHead.nextSibling;
+    console.log(headingP);
+    console.log(headingBtn);
+    console.log(headingFirst.children);
+    console.log(headingSecond);
+    console.log(headingThird);
+    console.log(headingFourth);
+    console.log(heroMobHead);
+    console.log(heroMobP);
+    console.log(headingImg);
+
+    TweenMax.to(hero, 0, { css: { visibility: "visible" } });
+    // HEADING TEXT ANIMATION
+    tl.staggerFrom(
+      [
+        headingFirst.children,
+        headingSecond.children,
+        headingThird.children,
+        headingFourth.children,
+      ],
+      1,
+      {
+        y: 74,
+        ease: Power3.easeOut,
+        delay: 0.8,
+      },
+      0.15
+    );
+  });
   React.useEffect(() => {
     // header carousel
     const headerCarousel = document.querySelector(".carousel");
@@ -63,24 +112,50 @@ const Banner = () => {
   });
 
   return (
-    <section className="home">
+    <div className="hero" ref={(el) => (hero = el)}>
       <div className="row full-height">
         <div className="container full-height">
           <div className="carousel carousel-slider full-height">
             <div className="carousel-item">
               <div className="col s12 pad0 center-xs">
                 <div className="col s12 m6 l5 ">
-                  <div className="main-text-container">
-                    <h1>Amazing Experiences from Our Wonderful Customers</h1>
-                    <p className="hidden-xs">
+                  <div
+                    className="main-text-container"
+                    ref={(el) => (heroHeading = el)}
+                  >
+                    <h1>
+                      {/* Amazing Experiences from Our Wonderful Customers */}
+                      <div className="banner-heading">
+                        <div className="banner-heading-inner">Amazing</div>
+                      </div>
+                      <div className="banner-heading">
+                        <div className="banner-heading-inner">
+                          Experiences from
+                        </div>
+                      </div>
+                      <div className="banner-heading">
+                        <div className="banner-heading-inner">
+                          Our Wonderful
+                        </div>
+                      </div>
+                      <div className="banner-heading">
+                        <div className="banner-heading-inner">Customers</div>
+                      </div>
+                    </h1>
+                    <p className="hidden-xs sub-heading">
                       Here is what customers and vendors are saying about us,
                       you can share your stories with us too.
                     </p>
-                    <div className="btn hidden-xs btn-bigger">share story</div>
+                    <div className="btn hidden-xs btn-bigger heading-btn">
+                      share story
+                    </div>
                   </div>
                 </div>
                 <div className="col s12 m6 offset-m0 l7 offset-l0">
-                  <div className="rel right-align banner-home-img">
+                  <div
+                    className="rel right-align banner-home-img"
+                    ref={(el) => (heroImg = el)}
+                  >
                     <img
                       src={bannerTwo}
                       alt="banner-img-1"
@@ -88,12 +163,15 @@ const Banner = () => {
                     />
                   </div>
                 </div>
-                <div className="col s12 visible-xs">
-                  <p>
+                <div
+                  className="col s12 visible-xs"
+                  ref={(el) => (heroMobile = el)}
+                >
+                  <p className="sub-heading">
                     Here is what customers and vendors are saying about us, you
                     can share your stories with us too.
                   </p>
-                  <div className="btn btn-bigger">share story</div>
+                  <div className="btn btn-bigger heading-btn">share story</div>
                 </div>
               </div>
             </div>
@@ -130,7 +208,7 @@ const Banner = () => {
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
